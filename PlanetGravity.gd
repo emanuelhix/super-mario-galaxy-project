335d6ef89bddda982e8ff3168a3dab0081bad582
Parker Hix <parker.e.hix-1@ou.edu>
2023-10-13 19:17:59 -0500

    fixed issue with raycasting in the wrong direction

--- PlanetGravity.gd
+++ PlanetGravity.gd
@@ -2,16 +2,19 @@ extends StaticBody2D
 
 @export var character: CharacterBody2D = null
 @onready var raycast: RayCast2D = character.find_child("RayCast2D")
-	
-func _process(delta):
-	# Update the raycast position relative to the character
-	raycast.target_position = (self.position - character.position) # or use local position if needed
-	raycast.force_raycast_update()
+
+func _physics_process(delta):
+	var space_state = get_world_2d().direct_space_state
+	var query = PhysicsRayQueryParameters2D.create(character.global_position, self.global_position)
+	query.exclude = [character.find_child("CollisionShape")]
+	var result = space_state.intersect_ray(query)
+
 	#print("Raycast Position:", raycast.global_position)
 	#print("Collision Point:", raycast.get_collision_point())
-	#print("Collision Normal:", raycast.get_collision_normal())
-
-	var normal_angle = raycast.get_collision_normal().angle() + PI/2
-	character.transform = Transform2D(normal_angle, character.global_position)
-	character.up_direction = character.transform.y
-	character.force_update_transform()
+	#print("Collision Normal:", raycast.get_collision_normal()
+	
+	if result.get("collider"):
+		var normal_angle = result.get("normal").angle() + PI/2
+		character.transform = Transform2D(normal_angle, character.global_position)
+		character.up_direction = character.transform.y
+		character.force_update_transform()
